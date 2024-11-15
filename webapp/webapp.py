f32c8d881c410f9de44019946185b4cd8fadc65f
Jorgen <165498681+JorgenAerts@users.noreply.github.com>
2024-11-15 15:52:48 +0100

    added locations to webapp

--- webapp/webapp.py
+++ webapp/webapp.py
@@ -6,27 +6,27 @@ import requests
 # Create Flask instance
 app = Flask(__name__)
 
-URL = "https://my-json-server.typicode.com/JorgenAerts/OpdrachtJsonApiWebserver/sensors"
-response = requests.get(URL)  # send a request to the URL to get the sensors list
+URL1 = "https://my-json-server.typicode.com/JorgenAerts/OpdrachtJsonApiWebserver/sensors"
+response = requests.get(URL1)  # send a request to the URL to get the sensors list
 sensors = response.json()  # Puts the received json data in the sensors variable.
+
+URL2 = "https://my-json-server.typicode.com/JorgenAerts/OpdrachtJsonApiWebserver/locations"
+response = requests.get(URL2)  # send a request to the URL to get the locations list
+locations = response.json()  # Puts the received json data in the locations variable.
 
 # Homepage which links to the other pages
 @app.route('/')
 def home():
     return render_template("index.html")
 
+# Page with all sensors listed
 @app.route("/sensors")
 def get_sensors():
-    URL = "https://my-json-server.typicode.com/JorgenAerts/OpdrachtJsonApiWebserver/sensors"
-    response = requests.get(URL)  # send a request to the URL to get the sensors list
-    sensors = response.json()  # Puts the received json data in the sensors variable.
     return render_template("sensors.html", title="Sensors", sensors=sensors)
 
+#  Page with all locations listed
 @app.route("/locations")
 def get_locations():
-    URL = "https://my-json-server.typicode.com/JorgenAerts/OpdrachtJsonApiWebserver/locations"
-    response = requests.get(URL)  # send a request to the URL to get the sensors list
-    locations = response.json()  # Puts the received json data in the sensors variable.
     return render_template("locations.html", title="Locations", locations=locations)
 
 # Page with that lists the sensor data from the sensor with the given id
@@ -38,4 +38,15 @@ def sensor(id):
         return render_template("sensor.html", sensor=sensor) # render the template with the given sensor's data
     else:
         return "Sensor not found.", 404  # Error message
+    
+# Page with that lists the location with the given id
+@app.route("/locations/<int:id>")
+def location(id):    
+    # Loop through locations to find the one with the matching id
+    if id < len(locations):  # check if the id is valid.
+        location = locations[id]  # Access the location
+        return render_template("sensor.html", location=location) # render the template with the given location
+    else:
+        return "Sensor not found.", 404  # Error message    
+    
 app.run(host='0.0.0.0', port=5000)

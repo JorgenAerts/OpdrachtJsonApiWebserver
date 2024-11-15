d7fb3c6ef5332ea3a869a639b400dd5296098328
Jorgen <165498681+JorgenAerts@users.noreply.github.com>
2024-11-15 16:53:34 +0100

    web app updated

--- webapp/webapp.py
+++ webapp/webapp.py
@@ -1,11 +1,12 @@
-
-""" Code adapted from previous excercises"""
+""" Code adapted from previous exercises """
 
 from flask import Flask, render_template
 import requests
+
 # Create Flask instance
 app = Flask(__name__)
 
+# URLs to get the sensors and locations data
 URL1 = "https://my-json-server.typicode.com/JorgenAerts/OpdrachtJsonApiWebserver/sensors"
 response = requests.get(URL1)  # send a request to the URL to get the sensors list
 sensors = response.json()  # Puts the received json data in the sensors variable.
@@ -13,6 +14,13 @@ sensors = response.json()  # Puts the received json data in the sensors variable
 URL2 = "https://my-json-server.typicode.com/JorgenAerts/OpdrachtJsonApiWebserver/locations"
 response = requests.get(URL2)  # send a request to the URL to get the locations list
 locations = response.json()  # Puts the received json data in the locations variable.
+
+# Helper function to get the location name by its ID
+def get_location_name(location_id):
+    for location in locations:
+        if location['id'] == location_id:
+            return location['location']
+    return "Unknown Location"  # If no match is found
 
 # Homepage which links to the other pages
 @app.route('/')
@@ -22,31 +30,43 @@ def home():
 # Page with all sensors listed
 @app.route("/sensors")
 def get_sensors():
-    return render_template("sensors.html", title="Sensors", sensors=sensors)
+    # Render the template with sensors and locations data
+    return render_template("sensors.html", title="Sensors", sensors=sensors, locations=locations)
 
-#  Page with all locations listed
+# Page with all locations listed
 @app.route("/locations")
 def get_locations():
+    print(locations)  # Print the locations to verify they are correct
     return render_template("locations.html", title="Locations", locations=locations)
 
-# Page with that lists the sensor data from the sensor with the given id
+# Page that lists the sensor data from the sensor with the given id
 @app.route("/sensors/<int:id>")
-def sensor(id):    
+def sensor(id):
     # Loop through sensors to find the one with the matching id
-    if id < len(sensors):  # check if the id is valid.
+    if id < len(sensors):  # Check if the id is valid
         sensor = sensors[id]  # Access the sensor
-        return render_template("sensor.html", sensor=sensor) # render the template with the given sensor's data
+        location_name = get_location_name(sensor['location'])  # Get location name using location ID
+        sensor['location_name'] = location_name  # Add location name to sensor data
+        return render_template("sensor.html", sensor=sensor)  # Render the template with the given sensor's data
     else:
-        return "Sensor not found.", 404  # Error message
-    
-# Page with that lists the location with the given id
+        return "Sensor not found.", 404  # Error message if sensor not found
+
+# Page that lists the location with the given id
 @app.route("/locations/<int:id>")
-def location(id):    
+def location(id):
     # Loop through locations to find the one with the matching id
-    if id < len(locations):  # check if the id is valid.
+    if id < len(locations):  # Check if the id is valid
         location = locations[id]  # Access the location
-        return render_template("sensor.html", location=location) # render the template with the given location
+        # Get all sensors at this location
+        location_sensors = [sensor for sensor in sensors if sensor['location'] == location['id']]
+        
+        # Add location name to each sensor at this location
+        for sensor in location_sensors:
+            sensor['location_name'] = location['location']
+        
+        return render_template("location.html", location=location, sensors=location_sensors)  # Render the template with the location data and related sensors
     else:
-        return "Sensor not found.", 404  # Error message    
-    
+        return "Location not found.", 404  # Error message if location not found
+
+# Run the Flask app
 app.run(host='0.0.0.0', port=5000)

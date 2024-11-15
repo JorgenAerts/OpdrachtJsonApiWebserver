df5af03139c6f7d6cdb83d37ac1a03678ee713fc
Jorgen <165498681+JorgenAerts@users.noreply.github.com>
2024-11-15 08:38:04 +0100

    name changes

--- webapp/webapp.py
+++ webapp/webapp.py
@@ -6,6 +6,10 @@ import requests
 # Create Flask instance
 app = Flask(__name__)
 
+URL = "https://my-json-server.typicode.com/JorgenAerts/OpdrachtJsonApiWebserver/sensors"
+response = requests.get(URL)  # send a request to the URL to get the sensors list
+sensors = response.json()  # Puts the received json data in the sensors variable.
+
 # Homepage which links to the other pages
 @app.route('/')
 def home():
@@ -13,21 +17,16 @@ def home():
 
 @app.route("/sensors")
 def get_sensors():
-    URL = "https://my-json-server.typicode.com/JorgenAerts/OpdrachtJsonApiWebserver/sensors"
-    response = requests.get(URL)  # send a request to the URL to get the sensors list
-    sensors = response.json()  # Puts the received json data in the sensors variable.
+
     return render_template("sensors.html", title="Sensors", sensors=sensors)
 
 # Page with that lists the sensor data from the sensor with the given id
 @app.route("/sensors/<int:id>")
-def sensor(id):
-    URL = f"https://my-json-server.typicode.com/JorgenAerts/OpdrachtJsonApiWebserver/sensors/{id}"
-    response = requests.get(URL)  # send a request to the URL to get the sensors list
-    sensors = response.json()  # Puts the received json data in the sensors variable.
-    if id < len(sensors):  # Check if the given id is not over the number of sensors and thus invalid
-        sensor = sensors[id]  # Makes the given id's sensor a variable
-        return render_template("sensor.html", sensor=sensor )  # Render the template for the sensor page
+def sensor(id):    
+    # Loop through sensors to find the one with the matching id
+    if id < len(sensors):  # check if the id is valid.
+        sensor = sensors[id]  # Access the sensor
+        return render_template("sensor.html", sensor=sensor) # render the template with the given sensor's data
     else:
-        return "No sensors found .", 404  # Error message
-    
+        return "Sensor not found.", 404  # Error message
 app.run(host='0.0.0.0', port=5000)
